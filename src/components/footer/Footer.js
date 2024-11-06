a563ab8d49a34025ff07eff0cfbb721b96db0dc7
FreelancerNics <infantcalvin2003@gmail.com>
2024-11-06 22:11:28 +0530

    Update Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -8,7 +8,7 @@ export default function Footer(props) {
     <div className="footer-div">
       <Fade>
         <p className="footer-text" style={{ color: props.theme.secondaryText }}>
-          Made with <span role="img">❤️</span> by Calvin © 2024 
+          Made with by Calvin © 2024 
         </p>
         {/* <ToggleSwitch theme={props.theme} onToggle={props.onToggle}/> */}
       </Fade>
